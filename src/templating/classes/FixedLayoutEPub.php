44629242806a2240246100f464811c2a8ce85f97
kimsia <kimcity@gmail.com>
2013-03-14 14:01:45 +0800

    Added UtilityLib as Plugin

--- src/templating/classes/FixedLayoutEPub.php
+++ src/templating/classes/FixedLayoutEPub.php
@@ -1,10 +1,47 @@
 <?php
-
+require_once 'FixedLayoutEPubPage.php';
 
 class FixedLayoutEPub {
 
+	// contains FixedLayoutEPubPage objects 
 	private $pages = array();
 
-	private $metadata = array();
+	private $metadata = array(); // hold string values only
 
+	public function __construct($metadata = array()) {
+		$this->_setDefaults();
+		$this->updateMetadata($metadata);
+	}
+
+	protected function _setDefaults() {
+		$this->metadata = array(
+			'title'		=> '',
+			'creator'	=> '',
+			'publisher'	=> '',
+			'date'		=> '',
+			'language'	=> 'en',
+			'book_id'	=> '',
+		);
+	}
+
+/**
+ *
+ * append the new values for metadata into the current metadata
+ */
+	public function updateMetadata($metadata) {
+		$this->metadata = array_merge($this->metadata, $metadata);
+	}
+
+/**
+ *
+ * append new page 
+ */
+	public function addPage(FixedLayoutEPubPage $page, $addAtIndex=null) {
+		if ($addAtIndex == null) {
+			// we just append to the pages
+			$this->pages[] = $page;
+		} else {
+
+		}
+	}
 }
--- src/templating/constants.php
+++ src/templating/constants.php
@@ -4,6 +4,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('APP', getcwd());
 
 define('BUILD', dirname(dirname(APP)) . DS . 'build');
+define('SRC', dirname(dirname(APP)) . DS . 'src');
 define('OUTPUT', BUILD . DS . 'output');
 define('SRC_EPUB_FILES', OUTPUT . DS . 'src_epub_files');
 define('EPUB_FILES', OUTPUT . DS . 'epub_files');
